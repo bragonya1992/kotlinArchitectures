ab3b849f85c055a139188b461049a17b9ec210a6
bragonya1992 <brayan.yaquian@fox.com>
2018-03-25 21:01:44 -0600

    First commit

---
+++ app/src/main/java/com/example/brayany/completeexampleunittest/interfaces/MainContract.kt
@@ -0,0 +1,15 @@
+package com.example.brayany.completeexampleunittest.interfaces
+
+/**
+ * Created by brayany on 3/25/2018.
+ */
+interface MainContract {
+    interface View {
+        fun showClickText()
+
+    }
+
+    interface UserAction {
+        fun createTopic(view: View)
+    }
+}
