b7b2b626d3220c9a3b322d2ea9fa6cc6a3ae4d86
bragonya1992 <brayan.yaquian@fox.com>
2018-03-26 00:20:29 -0600

    Add fab onClickListener and kotlin-extension is working

--- app/src/main/java/com/example/brayany/completeexampleunittest/MainActivity.kt
+++ app/src/main/java/com/example/brayany/completeexampleunittest/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.brayany.completeexampleunittest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.brayany.completeexampleunittest.interfaces.MainContract
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),MainContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener({ Log.d("press","fab")})
+    }
+
+    override fun showClickText() {
+        text.text = "press fab button"
     }
 }
